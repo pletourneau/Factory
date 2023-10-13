a9f1276b04bebb8ecaf33510e348c809209b0b3d
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-13 10:17:33 -0700

    WIP crud

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -38,19 +38,18 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-    //  public ActionResult Edit(int id)
-    // {
-    //   Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
-    //   return View(thisMachine);
-    // }
+     public ActionResult Edit(int id)
+    {
+      Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
 
-    // [HttpPost]
-    // public ActionResult Edit(Machine machine)
-    // {
-    //   _db.Machines.Update(machine);
-    //   _db.SaveChanges();
-    //   // return RedirectToAction("Index");
-    //   return RedirectToAction("Details", new { id = machine.MachineId });
-    // }
+    [HttpPost]
+    public ActionResult Edit(Machine machine)
+    {
+      _db.Machines.Update(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = machine.MachineId });
+    }
   }
 }
