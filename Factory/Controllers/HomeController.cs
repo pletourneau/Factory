57da275c0585f363ed73e6b2648a5b3074c90565
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-14 12:22:01 -0700

    add splash page with lists

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Factory.Controllers
+namespace ToDoList.Controllers
 {
-  public class HomeController : Controller
-  {
-    [HttpGet("/")]
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      return View();
+      private readonly FactoryContext _db;
+
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        Machine[] machinas = _db.Machines.ToArray();
+        Engineer[] nerds = _db.Engineers.ToArray();
+        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        model.Add("machines", machinas);
+        model.Add("engineers", nerds);
+        return View(model);
+      }
     }
-  }
 }
